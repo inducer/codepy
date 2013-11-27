57cb56e24d4574f887c65b9daed448cd03b3a43f
Andreas Kloeckner <inform@tiker.net>
2013-11-26 22:35:50 -0600

    Make test Py3-compatible

--- test/test_identical_symbols.py
+++ test/test_identical_symbols.py
@@ -14,12 +14,13 @@ def make_greet_mod(greeting):
     from codepy.toolchain import guess_toolchain
     return mod.compile(guess_toolchain(), wait_on_error=True)
 
+
 def test_identical_symbols():
     us = make_greet_mod("Hi there")
     aussie = make_greet_mod("G'day")
 
     assert us.greet() != aussie.greet()
-    print us.greet(), aussie.greet()
+    print(us.greet(), aussie.greet())
 
 if __name__ == '__main__':
     test_identical_symbols()
