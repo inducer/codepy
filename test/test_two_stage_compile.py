9414b3b3b9bd4c3084cbb713289699683887db29
arghdos <arghdos@gmail.com>
2017-09-06 15:38:11 -0400

    add test for two stage compile

---
+++ test/test_two_stage_compile.py
@@ -0,0 +1,33 @@
+from codepy.toolchain import guess_toolchain
+from codepy.jit import compile_from_string
+from ctypes import CDLL
+
+def test():
+    toolchain = guess_toolchain()
+
+    MODULE_CODE = """
+    extern "C" {
+        int const greet()
+        {
+            return 1;
+        }
+    }
+    """
+
+
+    # compile to object file
+    _, _, obj_path, _ = compile_from_string(toolchain, 'module', MODULE_CODE,
+                                            object=True)
+    # and then to shared lib
+    with open(obj_path, 'rb') as file:
+        obj = file.read()
+
+    _, _, ext_file, _ = compile_from_string(
+        toolchain, 'module', obj, source_name=['module.o'],
+        object=False, source_is_binary=True)
+
+    # test module
+    dll = CDLL(ext_file)
+    _fn = getattr(dll, 'greet')
+    _fn.restype = int
+    assert _fn() == 1
