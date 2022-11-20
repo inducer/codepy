36108d309e50b6d8cce0b8c68cb3bdca205ba1b7
Andreas Kloeckner <inform@tiker.net>
2022-11-20 00:31:41 -0600

    Drop codepy.cgen

--- codepy/cgen/__init__.py
+++
@@ -1,8 +0,0 @@
-from cgen import *  # noqa
-import cgen.cuda as cuda  # noqa: F401
-import cgen.opencl as opencl  # noqa: F401
-
-from warnings import warn as _warn
-_warn("codepy.cgen is deprecated. Use the separate 'cgen' module instead, see "
-        "the Python package index.", DeprecationWarning,
-        stacklevel=2)
