74a90530f73872511554943c33d25c2d600191ec
Alexandru Fikl <alexfikl@gmail.com>
2024-11-26 20:50:18 +0100

    mypy: strictly type tools

--- codepy/tools.py
+++ codepy/tools.py
@@ -3,18 +3,18 @@
 
 __copyright__ = "Copyright (C) 2009 Andreas Kloeckner"
 
+from collections.abc import Iterable
+from types import ModuleType
 
-def join_continued_lines(lines):
-    result = []
+
+def join_continued_lines(lines: Iterable[str]) -> list[str]:
+    result: list[str] = []
+
     it = iter(lines)
     append_line = False
     try:
         while True:
-            try:
-                line = it.next().rstrip("\n")
-            except AttributeError:
-                # py3
-                line = next(it).rstrip("\n")
+            line = next(it).rstrip("\n")
             append_next_line = line.endswith("\\")
             if append_next_line:
                 line = line[:-1]
@@ -27,12 +27,12 @@ def join_continued_lines(lines):
     except StopIteration:
         if append_line:
             from warnings import warn
-            warn("line continuation at end of file", stacklevel=2)
+            warn("Line continuation at end of file", stacklevel=2)
 
     return result
 
 
-def load_dynamic(name: str, path: str):
+def load_dynamic(name: str, path: str) -> ModuleType:
     """Implementation of ``imp.load_dynamic`` based on :mod:`importlib`."""
     # https://github.com/python/cpython/pull/105951
 
