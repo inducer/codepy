643ce991c7959ea4b9e8a6c780bfe569b852ec6b
Andreas Kloeckner <inform@tiker.net>
2024-12-28 18:30:51 +0100

    Eradicate wait_on_error

--- test/test_identical_symbols.py
+++ test/test_identical_symbols.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def make_greet_mod(greeting):
     from cgen import (
         Block,
@@ -23,12 +20,9 @@ def make_greet_mod(greeting):
                 ))
 
     from codepy.toolchain import guess_toolchain
-    return mod.compile(guess_toolchain(), wait_on_error=True)
+    return mod.compile(guess_toolchain())
 
 
-@pytest.mark.xfail(reason="You probably don't have "
-        "Boost.Python installed where I am looking for it, "
-        "and that's OK.")
 def test_identical_symbols():
     us = make_greet_mod("Hi there")
     aussie = make_greet_mod("G'day")
