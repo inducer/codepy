63f8638f32ace10a14f8c1a7d16ee1c67b1a71f8
Alexandru Fikl <alexfikl@gmail.com>
2025-01-09 21:26:09 +0200

    mypy: stricly type tests

--- test/test_identical_symbols.py
+++ test/test_identical_symbols.py
@@ -1,7 +1,9 @@
+from types import ModuleType
+
 import pytest
 
 
-def make_greet_mod(greeting):
+def make_greet_mod(greeting: str) -> ModuleType:
     from cgen import (
         Block,
         Const,
@@ -19,7 +21,7 @@ def make_greet_mod(greeting):
     mod.add_function(
             FunctionBody(
                 FunctionDeclaration(Const(Pointer(Value("char", "greet"))), []),
-                Block([Statement('return "%s"' % greeting)])
+                Block([Statement(f'return "{greeting}"')])
                 ))
 
     from codepy.toolchain import guess_toolchain
@@ -29,7 +31,7 @@ def make_greet_mod(greeting):
 @pytest.mark.xfail(reason="You probably don't have "
         "Boost.Python installed where I am looking for it, "
         "and that's OK.")
-def test_identical_symbols():
+def test_identical_symbols() -> None:
     us = make_greet_mod("Hi there")
     aussie = make_greet_mod("G'day")
 
